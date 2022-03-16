13638dfe4e94b278ab49bf7bdbb0f09578bd6127
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-03-16 16:41:13 +0530

    Code for 413 error code

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const GitHubStrategy = require("passport-github").Strategy;
 var LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 // const { CityLite } = require('country-state-city-js')
 //Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(
   session({
     secret: "secret",
